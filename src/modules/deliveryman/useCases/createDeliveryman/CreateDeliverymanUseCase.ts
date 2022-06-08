643e8f058df2b2c702da46097931040052623867
lopessoftec <lopessoftec@gmail.com>
2022-06-07 23:20:15 -0300

    Criado cadastro de deliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from "bcrypt";
+
+interface ICreateDeliveryman{
+    username: string;
+    password: string;
+}
+
+export class CreateDeliverymanUseCase {
+    async execute({ username, password }: ICreateDeliveryman){
+        // Validar se o deliveryman existe
+        const deliverymanExist = await prisma.deliveryman.findFirst({
+            where: {
+                username: {
+                    mode: "insensitive"
+                }
+            }
+        })
+
+        if(deliverymanExist){
+            throw new Error("Deliveryman already exists")
+        }
+
+        // Criptografar a senha
+        const hashPassword = await hash(password, 10);
+
+        // Salvar o deliveryman
+        const deliveryman = await prisma.deliveryman.create({
+            data: {
+                username,
+                password: hashPassword
+            }
+        });
+
+        return deliveryman;
+    }
+}
