ae512860f32734dfed00d4f19084b4c91eec16e8
lopessoftec <lopessoftec@gmail.com>
2022-06-08 23:10:28 -0300

    Desenvolvido busca de entregas utilizando o token do cliente

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient
 import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
+import { FindAllDeliveriesController } from "./modules/clients/deliveries/FindAllDeliveriesController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/findAllAvailableController";
@@ -18,6 +19,7 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 const createDeliveryController = new CreateDeliveryController();
 const findAllAvailableController = new FindAllAvailableController();
 const updateDeliverymanController = new UpdateDeliverymanController();
+const findAllDeliveriesController = new FindAllDeliveriesController();
 
 routes.post("/client/authenticate/", authenticateClientController.handle);
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle);
@@ -30,5 +32,7 @@ routes.post("/delivery/", ensureAuthenticateClient, createDeliveryController.han
 routes.get("/delivery/available", ensureAuthenticateDeliveryman, findAllAvailableController.handle);
 
 routes.put("/delivery/updateDeliveryman/:id", ensureAuthenticateDeliveryman, updateDeliverymanController.handle);
+
+routes.get("/client/deliveries", ensureAuthenticateClient, findAllDeliveriesController.handle);
 
 export { routes };
