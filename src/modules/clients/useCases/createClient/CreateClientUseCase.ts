5901c320050cfd0e569541c2d67835f52a208eeb
lopessoftec <lopessoftec@gmail.com>
2022-06-08 23:35:01 -0300

    Correcao da criacao de cliente e entregador e realizado busca com select

--- src/modules/clients/useCases/createClient/CreateClientUseCase.ts
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -13,6 +13,7 @@ export class CreateClientUseCase {
         const clientExist = await prisma.clients.findFirst({
             where: {
                 username: {
+                    equals: username,
                     mode: "insensitive"
                 }
             }
